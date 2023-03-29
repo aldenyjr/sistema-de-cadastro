f2525806860a0d8a93eb43fdaa91a95904096eeb
Aldeny Jr <114154072+aldenyjr@users.noreply.github.com>
2023-03-29 14:17:24 -0300

    testando o git commit

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log("Rodando do sistema de gerenciamento de jogos")
-console.log("arquivo atualizado para testar o comando git pull")
+console.log("Rodando do sistema de gerenciamento de jogos");
+console.log("arquivo atualizado para testar o comando git pull");
+console.log("testando o git commit");
