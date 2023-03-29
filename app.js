c5997b29dccfa15bb2adc72e4fe341f338b40e14
Aldeny Jr <114154072+aldenyjr@users.noreply.github.com>
2023-03-29 15:58:58 -0300

    retornando versao - 18d2c28

--- app.js
+++ app.js
@@ -3,4 +3,3 @@ console.log("arquivo atualizado para testar o comando git pull");
 console.log("testando o git commit");
 console.log("testando o git pull");
 console.log('Testando novamente o commit git commit . -m "mensagem"');
-console.log("TESTANDO NOVO COMMIT");
