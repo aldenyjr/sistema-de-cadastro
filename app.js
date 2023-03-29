19cdcf0c40cbd528dc54698cdd4903d629d1af7c
Aldeny Jr <114154072+aldenyjr@users.noreply.github.com>
2023-03-29 14:52:32 -0300

    voltando versao commit anterior

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando do sistema de gerenciamento de jogos");
 console.log("arquivo atualizado para testar o comando git pull");
 console.log("testando o git commit");
-console.log("testando o git pull");
-console.log('Testando novamente o commit git commit . -m "mensagem"');
