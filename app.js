fbc33a69b037dd8f8eaa983ad669a8604aaedf4f
Aldeny Jr <114154072+aldenyjr@users.noreply.github.com>
2023-03-29 15:56:46 -0300

    praticando commit e push

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("arquivo atualizado para testar o comando git pull");
 console.log("testando o git commit");
 console.log("testando o git pull");
 console.log('Testando novamente o commit git commit . -m "mensagem"');
+console.log("TESTANDO NOVO COMMIT");
